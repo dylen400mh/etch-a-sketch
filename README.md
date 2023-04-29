1efdf0e2dcdf76183743d472e32f6f6af960d6cd
Dylen <dylenbelanger2004@gmail.com>
2023-04-28 20:36:06 -0400

    Implement 'new grid' button to change size of grid, edit README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # etch-a-sketch
+
+This is a simple implementation of an etch-a-sketch program. 
+
+The current version allows the user to draw on the grid and change the grid-size to meet their desires.
+
+Future implementations of this project may include options to change the colour, as well as clearing the grid. 
--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-
 // creates 16x16 grid
+function createGrid(size) {
+
+    removeGrid();
+
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+
+        // square turns red if mouse is held
+        square.addEventListener("mouseover", () => {
+            if (isMouseDown) {
+                square.classList.add("trail");
+            }
+        })
+
+        container.appendChild(square);
+    }
+
+    // subtract 2 to account for borders
+    const squareSize = (800 / size) - 2;
+
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+    })
+}
+
+function removeGrid() {
+    //remove existing grid
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
 const container = document.querySelector(".container");
 
 let isMouseDown = false;
@@ -11,17 +45,14 @@ container.addEventListener("mouseup", () => {
     isMouseDown = false;
 })
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    
-    // square turns red if mouse is held down
-    square.addEventListener("mouseover", () => {
-        if (isMouseDown) {
-            square.classList.add("trail");
-        }
-    })
-    container.appendChild(square);
-}
+const newGridButton = document.querySelector(".new-grid-button");
 
+newGridButton.addEventListener("click", () => {
+    let input = prompt("Enter the new size of the grid.");
+    let size = parseInt(input);
 
+    createGrid(size);
+})
+
+// creates intial 16x16 grid
+createGrid(16);
